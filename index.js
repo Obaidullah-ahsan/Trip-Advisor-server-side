d95b00982f1e6929c685328bbc69a6aaa76a1555
Obaidullah-ahsan <abir60303@gmail.com>
2024-06-02 16:43:42 +0600

    review add database and get spacific guide review

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ async function run() {
       res.send(result);
     });
 
+    // reviews related api
+    app.get("/reviews/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { guide_email: email };
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
