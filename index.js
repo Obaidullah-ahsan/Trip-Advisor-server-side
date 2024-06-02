5ab72bb90bdf104499f1155d31c42536ce8afa3a
Obaidullah-ahsan <abir60303@gmail.com>
2024-06-02 16:15:48 +0600

    added packges and guides related api

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkk0rbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,6 +24,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const packagesCollection = client
+      .db("tripAdvisorDB")
+      .collection("packages");
+
+    const guidesCollection = client.db("tripAdvisorDB").collection("guides");
+
+    const reviewCollection = client.db("tripAdvisorDB").collection("reviews");
+
+    // pcckages related api
+    app.get("/packages", async (req, res) => {
+      const result = await packagesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // guides related api
+    app.get("/guides", async (req, res) => {
+      const result = await guidesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/guides/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await guidesCollection.findOne(query);
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
